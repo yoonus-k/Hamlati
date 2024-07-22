eafe7d0372cd1004fa4feca7e3c5e3bb6e011ddf
YOONUS KIZHAKKETHIL <131618962+yoonus-k@users.noreply.github.com>
2024-07-22 07:44:57 +0300

    main page and form created

---
+++ App/resources/views/form.blade.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--tw-bg-opacity: 1;background-color:rgb(255 255 255 / var(--tw-bg-opacity))}.bg-gray-100{--tw-bg-opacity: 1;background-color:rgb(243 244 246 / var(--tw-bg-opacity))}.border-gray-200{--tw-border-opacity: 1;border-color:rgb(229 231 235 / var(--tw-border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{--tw-shadow: 0 1px 3px 0 rgb(0 0 0 / .1), 0 1px 2px -1px rgb(0 0 0 / .1);--tw-shadow-colored: 0 1px 3px 0 var(--tw-shadow-color), 0 1px 2px -1px var(--tw-shadow-color);box-shadow:var(--tw-ring-offset-shadow, 0 0 #0000),var(--tw-ring-shadow, 0 0 #0000),var(--tw-shadow)}.text-center{text-align:center}.text-gray-200{--tw-text-opacity: 1;color:rgb(229 231 235 / var(--tw-text-opacity))}.text-gray-300{--tw-text-opacity: 1;color:rgb(209 213 219 / var(--tw-text-opacity))}.text-gray-400{--tw-text-opacity: 1;color:rgb(156 163 175 / var(--tw-text-opacity))}.text-gray-500{--tw-text-opacity: 1;color:rgb(107 114 128 / var(--tw-text-opacity))}.text-gray-600{--tw-text-opacity: 1;color:rgb(75 85 99 / var(--tw-text-opacity))}.text-gray-700{--tw-text-opacity: 1;color:rgb(55 65 81 / var(--tw-text-opacity))}.text-gray-900{--tw-text-opacity: 1;color:rgb(17 24 39 / var(--tw-text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--tw-bg-opacity: 1;background-color:rgb(31 41 55 / var(--tw-bg-opacity))}.dark\:bg-gray-900{--tw-bg-opacity: 1;background-color:rgb(17 24 39 / var(--tw-bg-opacity))}.dark\:border-gray-700{--tw-border-opacity: 1;border-color:rgb(55 65 81 / var(--tw-border-opacity))}.dark\:text-white{--tw-text-opacity: 1;color:rgb(255 255 255 / var(--tw-text-opacity))}.dark\:text-gray-400{--tw-text-opacity: 1;color:rgb(156 163 175 / var(--tw-text-opacity))}.dark\:text-gray-500{--tw-text-opacity: 1;color:rgb(107 114 128 / var(--tw-text-opacity))}}
+        </style>
+
+@vite('resources/css/app.css')
+@vite('resources/js/app.js')
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+  <!-- component -->
+<!-- Code on GiHub: https://github.com/vitalikda/form-floating-labels-tailwindcss -->
+<style>
+    .-z-1 {
+      z-index: -1;
+    }
+  
+    .origin-0 {
+      transform-origin: 0%;
+    }
+  
+    input:focus ~ label,
+    input:not(:placeholder-shown) ~ label,
+    textarea:focus ~ label,
+    textarea:not(:placeholder-shown) ~ label,
+    select:focus ~ label,
+    select:not([value='']):valid ~ label {
+      /* @apply transform; scale-75; -translate-y-6; */
+      --tw-translate-x: 0;
+      --tw-translate-y: 0;
+      --tw-rotate: 0;
+      --tw-skew-x: 0;
+      --tw-skew-y: 0;
+      transform: translateX(var(--tw-translate-x)) translateY(var(--tw-translate-y)) rotate(var(--tw-rotate))
+        skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+      --tw-scale-x: 0.75;
+      --tw-scale-y: 0.75;
+      --tw-translate-y: -1.5rem;
+    }
+  
+    input:focus ~ label,
+    select:focus ~ label {
+      /* @apply text-black; left-0; */
+      --tw-text-opacity: 1;
+      color: rgba(0, 0, 0, var(--tw-text-opacity));
+      left: 0px;
+    }
+  </style>
+  
+  <div class="min-h-screen bg-gray-100 p-0 sm:p-12">
+    <div class="mx-auto max-w-md px-6 py-12 bg-white border-0 shadow-lg sm:rounded-3xl">
+      <h1 class="text-2xl font-bold mb-8">Hajj Details</h1>
+      <form id="form" novalidate>
+        <div class="relative z-0 w-full mb-5">
+          <input
+            type="text"
+            name="name"
+            placeholder=" "
+            required
+            class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+          />
+          <label for="name" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Pilgrim ID</label>
+          <span class="text-sm text-red-600 hidden" id="error">Pilgrim ID Required</span>
+        </div>
+  
+        <div class="relative z-0 w-full mb-5">
+          <input
+            type="email"
+            name="email"
+            placeholder=" "
+            class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+          />
+          <label for="email" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">National ID/Iqama</label>
+          <span class="text-sm text-red-600 hidden" id="error">National ID/Iqama is required</span>
+        </div>
+  
+        <div class="relative z-0 w-full mb-5">
+          <input
+            type="text"
+            name="name"
+            placeholder=" "
+            class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+          />
+          <label for="password" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Name</label>
+          <span class="text-sm text-red-600 hidden" id="error">Name is required</span>
+        </div>
+  
+        <fieldset class="relative z-0 w-full p-px mb-5">
+          <legend class="absolute text-gray-500 transform scale-75 -top-3 origin-0">Gender</legend>
+          <div class="block pt-3 pb-2 space-x-4">
+            <label>
+              <input
+                type="radio"
+                name="radio"
+                value="1"
+                class="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+              />
+              Male
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="radio"
+                value="2"
+                class="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+              />
+              Female
+            </label>
+          </div>
+          <span class="text-sm text-red-600 hidden" id="error">Option has to be selected</span>
+        </fieldset>
+        <div class="relative z-0 w-full mb-5">
+            <input
+              type="number"
+              name="Age"
+              placeholder=" "
+              class="pt-3 pb-2 pl-5 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+            />
+            
+            <label for="money" class="absolute duration-300 top-3  -z-1 origin-0 text-gray-500">Age</label>
+            <span class="text-sm text-red-600 hidden" id="error">Age is required</span>
+          </div>
+
+          <div class="relative z-0 w-full mb-5">
+            <input
+              type="text"
+              name="name"
+              placeholder=" "
+              class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+            />
+            <label for="password" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Nationality</label>
+            <span class="text-sm text-red-600 hidden" id="error">Nationality is required</span>
+          </div>
+  
+          <div class="relative z-0 w-full mb-5">
+            <input
+              type="text"
+              name="name"
+              placeholder=" "
+              class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+            />
+            <label for="password" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Group ID</label>
+            <span class="text-sm text-red-600 hidden" id="error">Group ID is required</span>
+          </div>
+        <div class="relative z-0 w-full mb-5">
+          <select
+            name="select"
+            value=""
+            onclick="this.setAttribute('value', this.value);"
+            class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none z-1 focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+          >
+            <option value="" selected disabled hidden></option>
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+            <option value="4">Option 4</option>
+            <option value="5">Option 5</option>
+          </select>
+          <label for="select" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Special Needs</label>
+          <span class="text-sm text-red-600 hidden" id="error">Option has to be selected</span>
+        </div>
+  
+        <div class="relative z-0 w-full mb-5">
+            <select
+              name="select"
+              value=""
+              onclick="this.setAttribute('value', this.value);"
+              class="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none z-1 focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+            >
+              <option value="" selected disabled hidden></option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+              <option value="3">Option 3</option>
+              <option value="4">Option 4</option>
+              <option value="5">Option 5</option>
+            </select>
+            <label for="select" class="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Medical Conditions</label>
+            <span class="text-sm text-red-600 hidden" id="error">Option has to be selected</span>
+          </div>
+       
+  
+        
+  
+       
+  
+        <button
+          id="button"
+          type="button"
+          class="w-full px-6 py-3 mt-3 text-lg text-white transition-all duration-150 ease-linear rounded-lg shadow outline-none bg-pink-500 hover:bg-pink-600 hover:shadow-lg focus:outline-none"
+        >
+            Submit
+        </button>
+      </form>
+    </div>
+  </div>
+  
+  <script>
+    'use strict'
+  
+    document.getElementById('button').addEventListener('click', toggleError)
+    const errMessages = document.querySelectorAll('#error')
+  
+    function toggleError() {
+      // Show error message
+      errMessages.forEach((el) => {
+        el.classList.toggle('hidden')
+      })
+  
+      // Highlight input and label with red
+      const allBorders = document.querySelectorAll('.border-gray-200')
+      const allTexts = document.querySelectorAll('.text-gray-500')
+      allBorders.forEach((el) => {
+        el.classList.toggle('border-red-600')
+      })
+      allTexts.forEach((el) => {
+        el.classList.toggle('text-red-600')
+      })
+    }
+  </script>
+    </body>
+    
+</html>
